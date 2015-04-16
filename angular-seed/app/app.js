63fbf95c45e4dbe257c1565476c0ac9ea749ce35
NicholasPatt <nick.patterson2@gmail.com>
2015-04-15 23:15:22 -0400

    Survey questions added

--- angular-seed/app/app.js
+++ angular-seed/app/app.js
@@ -72,5 +72,41 @@ $rootScope.showCarousel = true
 tgs.controller('surveyController', function($scope, $rootScope, $location) {
 $rootScope.showCarousel = false
 $scope.survey_page = 1;
+$scope.haircuts = [
+    ['1729642','-urban-grunge-portrait.jpg'],
+    ['9377769','-hair-styled-man.jpg'],
+    ['15414914','-african-man-portrait.jpg'],
+    ['18623773','-bespectacled-young-male-on-off-white-background.jpg'],
+    ['29765448','-portrait-of-a-young-man.jpg'],
+    ['42365852','-man-wearing-sunglasses-and-suit-dreadlocks.jpg'],
+    ['49328610','-cheerful-young-man-portrait.jpg'],
+    ['55326536','-sculptural-face-and-mohican-hairstyle.jpg'],
+    ['58450262','-turkish-thoughtful-young-man.jpg'],
+    ['58954580','-i-ve-got-a-reason-to-smile.jpg']
+    ]
+
+    $scope.shuffle = function (array) {
+  var currentIndex = array.length, temporaryValue, randomIndex ;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+
+$scope.haircuts = $scope.shuffle($scope.haircuts);
+    $scope.haircuts_list = [$scope.haircuts.slice(0,3), $scope.haircuts.slice(3,6), $scope.haircuts.slice(6,9), [$scope.haircuts[9]]];
+
+    console.log($scope.haircuts_list);
 
 });
